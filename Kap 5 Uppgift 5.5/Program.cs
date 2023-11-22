02518ad64b12016d96df8471a9b6136f89f4d5df
humy06001 <humy06001@E-PW0234KS>
2023-11-22 21:37:50 +0100

    Add project files.

---
+++ Kap 5 Uppgift 5.5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Uppgift5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            string[] answers = { "Ja, eller?", "NEJ", "Ingen chans!", "Du borde skaffa dig en hobby.", "Jag förstår inte.", "Vilken dum fråga!", "42 är lösningen.", "Fråga inte mig!", "Jag vet inte...", "Du slösar min tid!"};
+            Random random = new Random();
+
+            while (true)
+            {
+                Console.WriteLine("Skriv en fråga till den magiska biljardbollen! Om du skriver \"a\" så stängs programmet av.");
+               
+                //Stänga av?
+                if (Console.ReadLine().ToLower() == "a")
+                {
+                    break; 
+                }
+
+                //Skriver ut ett slumpmässigt svar
+                Console.WriteLine(answers[random.Next(0, (answers.Length))]);
+            }
+        }
+    }
+}
+/*Skapa ett program som fungerar som leksaken ”Magic 8-ball”. Användaren ska få
+ställa hur många frågor hen vill till programmet som ska svara med slumpmässigt valt
+svar av 10 möjliga. Spara de 10 möjliga svaren i en array med strängar. Om
+användaren skriver in ”a” när hen ska ställa en fråga ska programmet avslutas.*/
